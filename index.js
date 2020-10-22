94659335d5cdb11be2b7cbd439cac861a59dbf2e
Philipp Rolinek <philipprolinek@gmail.com>
2020-10-22 18:54:44 +0200

    Added MongoDB for Server Prefix

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ require('module-alias/register');
 
 const path = require('path');
 const Commando = require('discord.js-commando');
+const { MongoClient } = require('mongodb');
+const MongoDBProvider = require('commando-provider-mongo');
 var time = new Date();
 
 const config = require('@root/config.json');
 const loadFeatures = require('@root/features/load-features');
-// const mongo = require('@util/mongo.js');
 
 // utils / features
 const memberCount = require('@features/member-count');
-// const { Mongoose } = require('mongoose');
 
 const client = new Commando.CommandoClient({
 	owner: '433645584696475653',
 	commandPrefix: config.prefix
 });
+
+client.setProvider(
+	MongoClient.connect(config.mongoPath)
+		.then((client) => {
+			return new MongoDBProvider(client, 'bot-db');
+		})
+		.catch((err) => {
+			console.error(err);
+		})
+);
 
 client.on('ready', async () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -31,18 +41,10 @@ client.on('ready', async () => {
 		])
 		.registerDefaultGroups()
 		.registerDefaultTypes({ string: true })
-		.registerDefaultCommands({ ping: false, prefix: false, eval: false, commandState: false })
+		.registerDefaultCommands({ ping: false, eval: false, commandState: false })
 		.registerCommandsIn(path.join(__dirname, 'cmds'));
 
 	client.user.setActivity('!help for commands');
-
-	// await mongo().then((mongoose) => {
-	// 	try {
-	// 		console.log('connected to mongo!');
-	// 	} finally {
-	// 		mongoose.connection.close();
-	// 	}
-	// });
 
 	loadFeatures(client);
 });
