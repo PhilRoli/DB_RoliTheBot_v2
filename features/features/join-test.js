bb90a9c1c3aabd838115e072f2ac78e633e551f2
Philipp Rolinek <philipprolinek@gmail.com>
2020-10-27 23:49:53 +0100

    Removed join test as it isnt needed anymore

--- features/features/join-test.js
+++
@@ -1,9 +0,0 @@
-module.exports = (client) => {
-	client.on('guildMemberAdd', (member) => {
-		console.log(`${member.id} has joined`);
-	});
-
-	client.on('guildMemberLeave', (member) => {
-		console.log(`${member.id} has left`);
-	});
-};
